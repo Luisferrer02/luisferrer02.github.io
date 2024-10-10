81205c336248a9f4241f3a81876f9f20c9b63037
Luisferrer02 <123595787+Luisferrer02@users.noreply.github.com>
2024-10-10 12:28:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ let currentIndex = 0;
 // Array de nombres de las fotos (actualiza las rutas para que apunten a la carpeta img)
 const photos = [];
 const texts = [
-    "Eres mi primer pensamiento al despertar.",
-    "Tu sonrisa ilumina mis días.",
-    "Eres mi razón de ser.",
-    "Cada momento contigo es un regalo.",
-    "Te amo más que a nada en este mundo.",
-    "Contigo, cada día es especial.",
-    "Eres la estrella que ilumina mi vida.",
-    "Nuestro amor es eterno.",
-    "Eres mi sueño hecho realidad.",
-    "Siempre estaré a tu lado.",
-    "Juntos, hacemos la mejor pareja.",
-    "Te elijo hoy y siempre.",
-    "Gracias por ser tú."
+    "Tu sonrisa que me ilumina y me hace feliz.",
+    "Lo bien que lo paso contigo siempre haciendo cosas divertidas.",
+    "Como me cuidas y me alegras el dia todos los dias que te veo.",
+    "Que eres una chica super inteligente con la que puedo hablar de todo.",
+    "Que me quieres por como soy y haces todo para que yo esté bien.",
+    "Que eres super detallista conmigo.",
+    "Que te preocupas por mi y te esfuerzas por siempre saber lo que hago.",
+    "Que me acompañas de viaje por todo el mundo y eres mi passenger princess.",
+    "Que sabes calmarme cuando me pongo nervioso.",
+    "Que me das todo tu amor todos los dias.",
+    "Que siempre estas conmigo aunque tengamos que hacer algo que no te apetece.",
+    "Por dar tu tiempo por mi, quedarte despierta cuando llego tarde o arreglarte con mis horarios para estar juntos.",
+    "Por creer en mi y apoyarme siempre."
 ];
 
 // Generar nombres de archivos de fotos con la nueva ruta
@@ -57,7 +57,7 @@ function showSlide(index) {
 // Intervalo para cambiar de imagen cada 3 segundos
 setInterval(() => {
     showSlide(currentIndex + 1);
-}, 3000);
+}, 4500);
 
 // Swipe para dispositivos móviles
 let startX = 0;
